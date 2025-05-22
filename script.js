65adc8ded875c76b3fc13a1f5685073ef39f36f5
Abimzz <olanipekunabim@gmail.com>
2025-05-22 22:47:36 +0100

    help update

--- script.js
+++ script.js
@@ -7,42 +7,78 @@ const successCard = document.getElementById('successCard');
 const userEmail = document.getElementById('userEmail');
 const dismissBtn = document.getElementById('dismissBtn');
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const emailValue = emailInput.value.trim();
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailValue || !emailRegex.test(emailValue)) {
-    errorMsg.style.display = 'block';
-    emailInput.focus();
-    } else {
-    errorMsg.style.display = 'none';
-    // Insert submitted email into success message
-    userEmail.textContent = emailValue;
-
-    // Hide form and show success card
-    formCard.classList.add('dismiss');
-    successCard.classList.add('active');
-    }
+submitBtn.addEventListener('click', () => {
+    signupForm.classList.add('dismiss');
+    successMessage.classList.add('active');
 });
 
 dismissBtn.addEventListener('click', () => {
-  // Reset form and show form card again
-    emailInput.value = '';
-    formCard.classList.remove('active');
-    successCard.classList.remove('dismiss');
+    successMessage.classList.remove('active');
+    signupForm.classList.remove('dismiss');
 });
 
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     const emailValue = emailInput.value.trim();
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+//     if (!emailValue || !emailRegex.test(emailValue)) {
+//       errorMsg.style.display = 'block';
+//       emailInput.focus();
+//     } else {
+//       submitBtn.style.display = 'none';
+//       // Insert submitted email into success message
+//       userEmail.textContent = emailValue;
+
+//       // Hide form card and show success card
+//       formCard.classList.add('dismiss');
+//       successCard.classList.add('active');}
+//     })
+
+// submitBtn.addEventListener('click', function(event)  {
+//   event.preventDefault();
+//   const emailValue = emailInput.value.trim();
+//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+//   if (!emailValue || !emailRegex.test(emailValue)) {
+//     errorMsg.style.display = 'block';
+//     emailInput.focus();
+//   } else {
+//     errorMsg.style.display = 'none';
+//     userEmail.textContent = emailValue;
+
+//   formCard.classList.add('dismiss');
+//   successCard.classList.add('active');}
+// })
 
 
 
+// submitBtn.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     const emailValue = emailInput.value.trim();
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-// submitBtn.addEventListener('click', () => {
-//     signupForm.classList.add('dismiss');
-//     successMessage.classList.add('active');
+//     if (!emailValue || !emailRegex.test(emailValue)) {
+//     errorMsg.style.display = 'block';
+//     emailInput.focus();
+//     } else {
+//     errorMsg.style.display = 'none';
+
+//     userEmail.textContent = emailValue;
+
+    
+//     formCard.classList.add('dismiss');
+//     successCard.classList.add('active');
+//     }
 // });
 
 // dismissBtn.addEventListener('click', () => {
-//     successMessage.classList.remove('active');
-//     signupForm.classList.remove('dismiss');
-// })
+//     emailInput.value = '';
+//     formCard.classList.remove('active');
+//     successCard.classList.remove('dismiss');
+// });
+
+
+
+
+
