93fdefa5af78f1f041988a6d0c90406a894e04ca
Abimzz <olanipekunabim@gmail.com>
2025-05-24 10:14:09 +0100

    fix to js submit

--- script.js
+++ script.js
@@ -7,33 +7,41 @@ const successCard = document.getElementById('successCard');
 const userEmail = document.getElementById('userEmail');
 const dismissBtn = document.getElementById('dismissBtn');
 
-submitBtn.addEventListener('click', () => {
-    signupForm.classList.add('dismiss');
-    successMessage.classList.add('active');
+// submitBtn.addEventListener('click', () => {
+//     signupForm.classList.add('dismiss');
+//     successMessage.classList.add('active');
+// });
+
+// dismissBtn.addEventListener('click', () => {
+//     successMessage.classList.remove('active');
+//     signupForm.classList.remove('dismiss');
+// });
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const emailValue = emailInput.value.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailValue || !emailRegex.test(emailValue)) {
+      errorMsg.style.display = 'block';
+      emailInput.focus();
+    } else {
+      errorMsg.style.display = 'none';
+
+      // Insert submitted email into success message
+      userEmail.textContent = emailValue;
+
+      // Hide form card and show success card
+      formCard.classList.add('dismiss');
+      successCard.classList.add('active');}
 });
 
 dismissBtn.addEventListener('click', () => {
-    successMessage.classList.remove('active');
-    signupForm.classList.remove('dismiss');
+    emailInput.value = '';
+    formCard.classList.remove('dismiss');
+    successCard.classList.remove('active');
 });
 
-// form.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     const emailValue = emailInput.value.trim();
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-//     if (!emailValue || !emailRegex.test(emailValue)) {
-//       errorMsg.style.display = 'block';
-//       emailInput.focus();
-//     } else {
-//       submitBtn.style.display = 'none';
-//       // Insert submitted email into success message
-//       userEmail.textContent = emailValue;
-
-//       // Hide form card and show success card
-//       formCard.classList.add('dismiss');
-//       successCard.classList.add('active');}
-//     })
 
 // submitBtn.addEventListener('click', function(event)  {
 //   event.preventDefault();
@@ -72,11 +80,7 @@ dismissBtn.addEventListener('click', () => {
 //     }
 // });
 
-// dismissBtn.addEventListener('click', () => {
-//     emailInput.value = '';
-//     formCard.classList.remove('active');
-//     successCard.classList.remove('dismiss');
-// });
+
 
 
 
